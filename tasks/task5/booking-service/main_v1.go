4b2dadad1da0f6795f2c2cbeaf6245cc94d82275
Anton Egorov <aegorov@aligntech.com>
2025-11-28 11:11:40 +0300

    [task5] Two versions of service

---
+++ tasks/task5/booking-service/main_v1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "os"
+)
+
+func main() {
+  enableFeatureX := os.Getenv("ENABLE_FEATURE_X") == "true"
+
+  http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "pong")
+  })
+
+  http.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "ready")
+  })
+
+  // if ENABLE_FEATURE_X=true, expose /feature
+  if enableFeatureX {
+    http.HandleFunc("/feature", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "Feature X is enabled!")
+    })
+  }
+
+  log.Println("Server v1 running on :8080")
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ tasks/task5/booking-service/main_v2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "strings"
+)
+
+func main() {
+  http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "pong")
+  })
+
+  http.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "ready")
+  })
+
+  // /feature доступен всегда, но отвечает только если заголовок X-Feature-Enabled: true
+  http.HandleFunc("/feature", func(w http.ResponseWriter, r *http.Request) {
+    enabled := strings.EqualFold(r.Header.Get("X-Feature-Enabled"), "true")
+    if !enabled {
+      w.WriteHeader(http.StatusForbidden)
+      fmt.Fprintf(w, "Feature X is disabled. Set header X-Feature-Enabled: true")
+      return
+    }
+    fmt.Fprintf(w, "Feature X is enabled via header!")
+  })
+
+  log.Println("Server v2 running on :8080")
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
